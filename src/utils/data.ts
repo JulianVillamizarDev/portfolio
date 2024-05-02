8ec5781fea0ed5086b9272c455bea6c07a23932d
Julian Villamizar <julianvillamizardev@gmail.com>
2024-05-01 21:39:25 -0500

    projects data, responsive design and tailwind config updated

---
+++ src/utils/data.ts
@@ -0,0 +1,51 @@
+export const educationTimeline = [
+	{
+		title: 'Francisco de Paula Santander university.',
+		date: '2021 - Present',
+		description: 'Systems Engineering.'
+	},
+	{
+		title: 'Colegio Maria Reina.',
+		date: '2013 - 2019',
+		description: 'Highschool degree.'
+	}
+];
+
+export const experienceTimeline = [
+	{
+		title: 'Consultorio Odontologico Especializado Angela Villamizar',
+		date: 'Aug 2023 - Jan 2024',
+		description: ''
+	},
+];
+
+export const projects = [
+	{
+		title: 'dentist landing page',
+		description: "Dentist landing page made with Astro, React and TailwindCSS.",
+		href: 'https://odangelavillamizar.vercel.app/',
+	},
+];
+
+export const socials = [
+    {
+        text: 'LinkedIn',
+        icon: 'github',
+		href: 'https://github.com/JulianVillamizarDev'
+    },
+    {
+        text: 'GitHub',
+        icon: 'linkedin',
+		href: 'https://www.linkedin.com/in/julian-villamizar-a0a94526a'
+    },
+	{
+        text: 'Instagram',
+		icon: 'instagram',
+        href: 'https://www.linkedin.com/in/julian-villamizar-a0a94526a',
+    },
+    {
+        text: 'X',
+		icon: 'x',
+        href: 'https://www.linkedin.com/in/julian-villamizar-a0a94526a',
+    },
+];
